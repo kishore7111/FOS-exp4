371f55b7f65c0e954898e4165026ede82e84c055
Kishore S <kishores.cs22@bitsathy.ac.in>
2024-11-05 09:05:42 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+// src/app.js
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const taskSchema = require('./schema/taskSchema');
+dotenv.config();
+const app = express();
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error(err));
+// Setup GraphQL endpoint
+app.use('/graphql', graphqlHTTP({
+  schema: taskSchema,
+  graphiql: true
+}));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
